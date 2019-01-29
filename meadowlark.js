6d8fb69e4685e87e5e88a86dcf3fc0d7b2238ccb
dddw001 <2410656679@qq.com>
2019-01-29 15:54:21 +0800

    局部文件&段落&form表单post提交

--- meadowlark.js
+++ meadowlark.js
@@ -1,16 +1,40 @@
 var express = require('express')
 var fortune = require('./lib/fortune.js')
+var weather = require('./lib/weather.js')
 
 var app = express()
+app.set('x-powered-by', false)
 
 app.set('port', process.env.PORT || 3000)
 // 设置模板引擎
-var handlebars = require('express3-handlebars').create({defaultLayout: 'main'})
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', 'handlebars')
+var handlebars = require('express3-handlebars').create({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) {
+        this._sections = {}
+      }
+      this._sections[name] = options.fn(this)
+      return null
+    }
+  }
+})
+app.engine('hbs', handlebars.engine)
+app.set('view engine', 'hbs')
 
 // 设置静态资源目录
 app.use(express.static(__dirname + '/public'))
+
+app.use(require('body-parser').urlencoded({extended: false}))
+
+app.use((req, res, next) => {
+  if (!res.locals.partials) {
+    res.locals.partials = {}
+  }
+  res.locals.partials.weather = weather.getWeatherData()
+  next()
+})
 
 // 首页
 app.get('/', function(req, res){
@@ -22,6 +46,44 @@ app.get('/', function(req, res){
 // about页
 app.get('/about', function(req, res){
   res.render('about', {fortune: fortune.getFortune()})
+})
+
+// 先渲染视图，再渲染布局
+app.get('/foo', (req, res) => {
+  res.render('foo', {layout: null})
+})
+
+app.get('/jquery-test', (req, res) => {
+  res.render('jquery-test')
+})
+
+// 在视图中使用hbs jquery 渲染页面
+app.get('/nursery-rhyme', (req, res) => {
+  res.render('nursery-rhyme')
+})
+
+app.get('/data/nursery-rhyme', (req, res) => {
+  res.json({
+    animal: 'squirrel',
+    bodyPart: 'tail',
+    adjective: 'bushy',
+    noun: 'heck'
+  })
+})
+
+// 提交表单
+app.get('/news-letter', function(req, res){
+  res.render('news-letter', {csrf: 'CSRF token goes here'})
+})
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form)
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf)
+  console.log('Name (from visible form field): ' + req.body.name)
+  console.log('Email (from visible form field): ' + req.body.email)
+  res.redirect(303, '/thank')
+})
+app.get('/thank', function(req, res){
+  res.send('Thank You')
 })
 
 // 404页面
