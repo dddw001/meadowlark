da76c273ead407a89a69803c5ed713484c36ad06
dddw001 <2410656679@qq.com>
2019-01-30 10:02:06 +0800

    表单ajax请求&会话

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var fortune = require('./lib/fortune.js')
 var weather = require('./lib/weather.js')
+var formidable = require('formidable')
+var credentials = require('./lib/credentials')
 
 var app = express()
 app.set('x-powered-by', false)
@@ -28,11 +30,18 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(require('body-parser').urlencoded({extended: false}))
 
+app.use(require('cookie-parser')(credentials.cookieSecret))
+app.use(require('express-session')())
+
 app.use((req, res, next) => {
   if (!res.locals.partials) {
     res.locals.partials = {}
   }
   res.locals.partials.weather = weather.getWeatherData()
+
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+
   next()
 })
 
@@ -84,6 +93,70 @@ app.post('/process', function(req, res){
 })
 app.get('/thank', function(req, res){
   res.send('Thank You')
+})
+
+// ajax处理表单
+app.get('/news-letter-ajax', function(req, res){
+  res.render('news-letter-ajax', {csrf: 'CSRF token goes here'})
+})
+
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb()
+}
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/
+
+app.post('/process2', function(req, res){
+  var name = req.body.name || ''
+  var email = req.body.email || ''
+  // 输入验证
+  if(!email.match(VALID_EMAIL_REGEX)) {
+    if(req.xhr) {
+      return res.json({ error: 'Invalid name email address.' })
+    }
+    req.session.flash = {
+      message: 'The email address you entered was not valid.'
+    }
+    return res.redirect(303, '/')
+  }
+  new NewsletterSignup({ name: name, email: email }).save(function(err){
+    if(err) {
+      if(req.xhr) {
+        return res.json({ error: 'Database error.' })
+      }
+      req.session.flash = {
+        message: 'There was a database error; please try again later.'
+      }
+      return res.redirect(303, '/')
+    }
+    if (req.xhr || req.accepts('json, html') === 'json') {
+      res.send({success: true})
+    } else {
+      res.redirect(303, '/')
+    }
+  })
+})
+
+// 上传文件
+app.get('/contest/vacation-photo',function(req,res){
+  var now = new Date()
+  res.render('contest/vacation-photo',{
+    year: now.getFullYear(),
+    month: now.getMonth()
+  })
+})
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+  var form = new formidable.IncomingForm()
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error')
+    console.log('received fields:')
+    console.log(fields)
+    console.log('received files:')
+    console.log(files)
+    res.redirect(303, '/thank')
+  })
 })
 
 // 404页面
