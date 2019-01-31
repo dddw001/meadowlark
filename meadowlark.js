fd54fb63fb131d4eb91be6a3cca078944bae3083
dddw001 <2410656679@qq.com>
2019-01-31 18:08:20 +0800

    使用Express提供API

--- meadowlark.js
+++ meadowlark.js
@@ -117,6 +117,9 @@ app.use(require('body-parser').urlencoded({extended: false}))
 app.use(require('cookie-parser')(credentials.cookieSecret))
 app.use(require('express-session')())
 
+// 以api开头的API可跨域
+app.use('/api', require('cors')())
+
 // 设置locals数据
 app.use((req, res, next) => {
   if (!res.locals.partials) {
@@ -267,6 +270,56 @@ app.get('/vacations', function (req, res) {
                   break
     }
     res.render('vacations', context)
+  })
+})
+
+// 景点的增删改查
+var Attraction = require('./models/attraction.js')
+app.get('/api/attractions', function (req, res) {
+  Attraction.find({ approved: true }, function(err, attractions){
+    if (err) {
+      return res.send(500, 'Error occurred: database error.')
+    }
+    res.json(attractions.map(function (a) {
+      return {
+        name: a.name,
+        id: a._id,
+        description: a.description,
+        location: a.location
+      }
+    }))
+  })
+})
+app.post('/api/attraction', function (req, res) {
+  var a = new Attraction({
+    name: req.body.name,
+    description: req.body.description,
+    location: { lat: req.body.lat, lng: req.body.lng },
+    history: {
+      event: 'created',
+      email: req.body.email,
+      date: new Date()
+    },
+    approved: false
+  })
+  a.save(function (err, a) {
+    if (err) {
+      return res.send(500, 'Error occurred: database error.')
+    }
+    res.json({ id: a._id })
+  })
+})
+app.get('/api/attraction/:id', function (req,res) {
+  Attraction.findById(req.params.id, function (err, a) {
+    if (err) {
+      return res.send(500, 'Error occurred: database error.')
+    }
+    res.json({
+      name: a.name,
+      id: a._id,
+      description: a.description,
+      location: a.location
+    })
   })
 })
 
